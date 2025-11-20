04bd86c705345823b3e44a492bf7e996c7b8d446
Jarancibia <arancibiajav@gmail.com>
2025-11-20 16:08:46 +0100

    feat: Log MongoDB connection details

--- src/utils/database.js
+++ src/utils/database.js
@@ -1,12 +1,14 @@
-const mongoose = require('mongoose');
-const logger = require('./logger');
+const mongoose = require("mongoose");
+const logger = require("./logger");
 
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI);
-    logger.info('MongoDB connected successfully');
+    logger.info("MongoDB connected successfully", {
+      conn: process.env.MONGODB_URI.split("@")[1],
+    });
   } catch (error) {
-    logger.error('MongoDB connection error:', error);
+    logger.error("MongoDB connection error:", error);
     process.exit(1);
   }
 };
